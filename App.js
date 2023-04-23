d25a723686167dfaf9f4bce76ca8b82dc12921ab
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2023-04-23 19:36:53 -0300

    Expo

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+
+// Necessário, pois o Expo tem o Input dentro do node_modules e o .gitignore volta para a configuração inicial
+import Initial from "./src/App";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Initial />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#282a36",
-    color: "#f8f8f2",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
