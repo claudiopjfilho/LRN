cc018cdcdc9ca5aadbb51ac6f31ea0230e0b73fe
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2023-05-13 21:00:23 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, StyleSheet } from "react-native";
-
+import Pai from "./components/Indireta/Pai";
+//import Pai from "./components/Direta/Pai";
 //import Primeiro from "./components/Primeiro";
 //import Aleatorio from "./components/Aleatorio";
-import Titulo from "./components/Titulo";
-import Botao from "./components/Botao";
-import Contador from "./components/Contador";
+//import Titulo from "./components/Titulo";
+//import Botao from "./components/Botao";
+//import Contador from "./components/Contador";
 
 export default () => (
   <View style={styles.container}>
+    {/*
     <Contador inicial={10} />
     <Contador inicial={20} passo={20} />
-    {/*
-    <Contador Numero={3} />
+    <Contador inicial={3} />
+
+    <Pai />
+
     <Botao />
     <Primeiro min={3} max={20} />
     <Primeiro min="3" max="20" />
---
+++ src/components/Indireta/Pai.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Button, Text } from "react-native";
+import Filho from "./Filho";
+
+export default props => {
+  return <Button title="executar" />;
+};
